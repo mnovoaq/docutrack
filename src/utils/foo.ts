d0a6128ba5e31a675d5e374f64bc0a3db91c0059
Mitchel Novoa <m.novoa@treck.local>
2026-06-23 11:57:34 -0400

    chore: eliminar archivos de test generados accidentalmente

--- src/utils/foo.ts
+++
@@ -1,9 +0,0 @@
-{
-  "pending": [
-    {
-      "file": "C:/Users/MADA7~1.NOV/AppData/Local/Temp/docutrack-test-tY6Z8g/queue.json",
-      "addedAt": "2026-06-23T15:54:00.636Z"
-    }
-  ],
-  "lastClear": null
-}
